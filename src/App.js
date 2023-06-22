4bb0f45064f3ced8371b0fd330335ee20af1477b
Louis Doan <louisdoan9@gmail.com>
2023-06-21 20:33:49 -0400

    Have background gradient slightly change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import NavBar from './Pages/NavBar/NavBar';
 import AboutMe from './Pages/AboutMe/AboutMe';
 import Projects from './Pages/Project/Projects';
+import { useState } from 'react';
 
 function App() {
+	const [opacity, setOpacity] = useState(1);
+
 	return (
 		<div className="App">
-			<NavBar />
-			<AboutMe />
-			<Projects />
+			<div className="App-overlay">
+				<NavBar />
+				<AboutMe />
+				<Projects />
+			</div>
 		</div>
 	);
 }
