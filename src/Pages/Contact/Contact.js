a8505b7aaaae21c0eba354133238808a1b1b0545
Louis Doan <louisdoan9@gmail.com>
2023-06-25 22:06:14 -0400

    Create and add css to contact page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,10 +1,37 @@
+import { useEffect } from 'react';
 import './Contact.css';
 
 function Contact() {
+	// reset input
+	useEffect(() => {
+		document.getElementById('name').value = '';
+		document.getElementById('email').value = '';
+		document.getElementById('message').value = '';
+	});
+
 	return (
 		<main className="contact" id="contact">
 			<section>
 				<h1>Contact.</h1>
+				<p>Email me at doanl7@mcmaster.ca</p>
+				<form action="https://api.web3forms.com/submit" method="POST">
+					<input type="hidden" name="access_key" value={process.env.REACT_APP_EMAIL_KEY} />
+					<div>
+						<label htmlFor="name">Name:</label>
+						<input type="text" name="name" id="name" required />
+					</div>
+					<div>
+						<label htmlFor="email">Email:</label>
+						<input type="email" name="email" id="email" required />
+					</div>
+					<div>
+						<label htmlFor="message">Message:</label>
+						<textarea name="message" id="message" required></textarea>
+					</div>
+					<div class="h-captcha" data-captcha="true"></div>
+					<button type="submit">Send Message</button>
+				</form>
+				<script src="https://web3forms.com/client/script.js" async defer></script>
 			</section>
 		</main>
 	);
